99b26924fb206a1116dc7c9a5a1637fee0bbad54
webmaxvianna <webmaxvianna@gmail.com>
2021-06-16 10:32:20 -0300

    Incluído Lista de Colaboradores

--- pages/visualizar_empresa.php
+++ pages/visualizar_empresa.php
@@ -71,7 +71,140 @@
       </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
-</div>
+
+    <section class="content">     
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">            
+            <p class="card-title">Lista de colaboradores</p>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body table-responsive p-0">
+            <table class="table table-bordered table-striped table-hover">
+              <thead>                  
+                <tr>
+                  <th>Nome</th>
+                  <th>CPF</th>                  
+                  <th>Empresa</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class=" text-nowrap">Ana Souza</td>
+                  <td class=" text-nowrap">123.456.789-00</td>
+                  <td class=" text-nowrap">FUNDAÇÃO INOVA PRUDENTE</td>
+                  <td class=" text-nowrap">
+                    <a href="ocorrencias.php" class="btn btn-info btn-sm center">
+                      <i class="fas fa-portrait"></i>&nbsp;
+                      Ocorrências
+                    </a>
+                    <a href="alterar_usuario.php" class="btn btn-warning btn-sm center">
+                      <i class="far fa-edit"></i>&nbsp;
+                      Alterar
+                    </a>
+                    <a href="#" class="btn btn-danger btn-sm center" onclick="return confirm('Você realmente quer excluir esse usuário?');">
+                      <i class="far fa-trash-alt"></i>&nbsp;
+                      Excluir
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td class=" text-nowrap">Jose Silva</td>
+                  <td class=" text-nowrap">987.654.321-22</td>
+                  <td class=" text-nowrap">FUNDAÇÃO INOVA PRUDENTE</td>
+                  <td class=" text-nowrap">
+                    <a href="ocorrencias.php" class="btn btn-info btn-sm center">
+                      <i class="fas fa-portrait"></i>&nbsp;
+                      Ocorrências
+                    </a>
+                    <a href="alterar_usuario.php" class="btn btn-warning btn-sm center">
+                      <i class="far fa-edit"></i>&nbsp;
+                      Alterar
+                    </a>
+                    <a href="#" class="btn btn-danger btn-sm center" onclick="return confirm('Você realmente quer excluir esse usuário?');">
+                      <i class="far fa-trash-alt"></i>&nbsp;
+                      Excluir
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td class=" text-nowrap"><a href="visualizar_usuario.php">Alexander Pierce</a></td>
+                  <td class=" text-nowrap">369.258.147-99</td>
+                  <td class=" text-nowrap"><a href="visualizar_empresa.php">FUNDAÇÃO INOVA PRUDENTE</a></td>
+                  <td class=" text-nowrap">
+                    <a href="ocorrencias.php" class="btn btn-info btn-sm center">
+                      <i class="fas fa-portrait"></i>&nbsp;
+                      Ocorrências
+                    </a>
+                    <a href="alterar_usuario.php" class="btn btn-warning btn-sm center">
+                      <i class="far fa-edit"></i>&nbsp;
+                      Alterar
+                    </a>
+                    <a href="#" class="btn btn-danger btn-sm center" onclick="return confirm('Você realmente quer excluir esse usuário?');">
+                      <i class="far fa-trash-alt"></i>&nbsp;
+                      Excluir
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td class=" text-nowrap">Ana Carolina Silva</td>
+                  <td class=" text-nowrap">369.258.147-99</td>
+                  <td class=" text-nowrap">FUNDAÇÃO INOVA PRUDENTE</td>
+                  <td class=" text-nowrap">
+                    <a href="ocorrencias.php" class="btn btn-info btn-sm center">
+                      <i class="fas fa-portrait"></i>&nbsp;
+                      Ocorrências
+                    </a>
+                    <a href="alterar_usuario.php" class="btn btn-warning btn-sm center">
+                      <i class="far fa-edit"></i>&nbsp;
+                      Alterar
+                    </a>
+                    <a href="#" class="btn btn-danger btn-sm center" onclick="return confirm('Você realmente quer excluir esse usuário?');">
+                      <i class="far fa-trash-alt"></i>&nbsp;
+                      Excluir
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td class=" text-nowrap">Luis Henrique Souza</td>
+                  <td class=" text-nowrap">369.258.147-99</td>
+                  <td class=" text-nowrap">FUNDAÇÃO INOVA PRUDENTE</td>
+                  <td class=" text-nowrap">
+                    <a href="ocorrencias.php" class="btn btn-info btn-sm center">
+                      <i class="fas fa-portrait"></i>&nbsp;
+                      Ocorrências
+                    </a>
+                    <a href="alterar_usuario.php" class="btn btn-warning btn-sm center">
+                      <i class="far fa-edit"></i>&nbsp;
+                      Alterar
+                    </a>
+                    <a href="#" class="btn btn-danger btn-sm center" onclick="return confirm('Você realmente quer excluir esse usuário?');">
+                      <i class="far fa-trash-alt"></i>&nbsp;
+                      Excluir
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <!-- /.card-body -->
+          <div class="card-footer clearfix">
+            <ul class="pagination pagination-sm m-0 float-right">
+              <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+              <li class="page-item"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- /.card -->
+      </div>
+    </section>
+    <!-- /.content -->
+
+  </div>
 <?php
   include ('../includes/footer.php');
 ?>
