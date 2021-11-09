d6fd237ec0f0a5a076f0d0a4c6a562c23d9b7ef4
webmaxvianna <webmaxvianna@gmail.com>
2021-11-09 09:29:39 -0300

    Meta tag para robots de buscadores

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- Informar aos buscadores para não indexar e não seguir nada dessa página -->
+  <meta name="robots" content="noindex, nofollow"> 
   <title>AdminLTE 3 | Project Add</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
